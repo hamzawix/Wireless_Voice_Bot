fc8a173b7f56b590ad94b4aec48347c0545f0c15
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-03-05 23:21:40 +0100

    adding the decide function that decides which way the robot should follow(no regex in use)

--- pycode/main.py
+++ pycode/main.py
@@ -6,18 +6,47 @@ Created on Mar 4, 2017
 
 import speech_recognition as sr
 
-#Getting audio from microphone
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Command me!")
-    audio = r.listen(source)
+def decide(order):
+    """
+    This is a function that takes the returned text from the recognizer
+    as an argument and decides where the robot should go.
+    """
+    if order == "stop":
+        print("stoping")
+        
+    elif order == "forward":
+        print("going forward")
+        
+    elif order == "backward":
+        print("going backward")
+        
+    elif order == "left":
+        print("turning left")
+        
+    elif order == "right":
+        print("turning right")
+        
+    else:
+        print("I did not understand")
 
-#Recognising speech
-try:
-    print("I think you said: {}".format(r.recognize_sphinx(audio)))
-          
-except sr.UnknownValueError:
-    print("I coudn't understand you!")
+
+if __name__ == '__main__':
     
-except sr.RequestError as e:
-    print("an error; {}".format(e))
+    #Getting audio from microphone
+    engine = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Command me!")
+        audio = engine.listen(source)
+    
+    #Recognising speech and deciding
+    try:
+        b = engine.recognize_sphinx(audio)
+        decide(b)
+        #print("I think you said: {}".format(b))
+              
+    except sr.UnknownValueError:
+        print("I coudn't understand you!")    
+    
+
+
+    
