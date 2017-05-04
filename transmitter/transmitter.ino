9dd33a76fd1e8b3479f86828cf22bebcee430c7c
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-05-04 22:36:07 +0100

    changes in arduino code

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,14 +1,4 @@
-/*
-  DigitalReadSerial
- Reads a digital input on pin 2, prints the result to the serial monitor
 
- This example code is in the public domain.
- */
-
-// digital pin 2 has a pushbutton attached to it. Give it a name:
-
-
-// the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -16,11 +6,13 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
 
+char v = ' ';
+
 if(Serial.available()){
-Serial.println(Serial.read());
+  v = Serial.read();
+Serial.println(v);
 }
 }
 
