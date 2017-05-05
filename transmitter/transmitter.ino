fb771475feb7d33861e28ba2823c1315c67e7f69
Hamza Boughraira <hamza.boughraira@gmail.com>
2017-05-05 12:39:09 +0100

    Motor code added

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,9 +1,19 @@
 
+  int m1_f = 3;
+  int m1_b = 4;
+  int m2_f = 5;
+  int m2_b = 6;
+  
 void setup() {
-  // initialize serial communication at 9600 bits per second:
+  
   Serial.begin(9600);
-  // make the pushbutton's pin an input:
-  pinMode(LED_BUILTIN, OUTPUT);
+ 
+  pinMode(m1_f, OUTPUT);
+  pinMode(m1_b, OUTPUT);
+  pinMode(m2_f, OUTPUT);
+  pinMode(m2_b, OUTPUT);
+
+  
 }
 
 void loop() {
@@ -12,7 +22,49 @@ char v = ' ';
 
 if(Serial.available()){
   v = Serial.read();
-Serial.println(v);
+  Serial.println(v);
+  if(v == 's'){
+    digitalWrite(m1_f, 0);
+    digitalWrite(m1_b, 0);
+    digitalWrite(m2_f, 0);
+    digitalWrite(m2_b, 0);
+  }
+  
+ else if(v == 'f'){
+  digitalWrite(m1_f, 1);
+  digitalWrite(m1_b, 0);
+  digitalWrite(m2_f, 1);
+  digitalWrite(m2_b, 0);
+ }
+ else if(v == 'b'){
+  digitalWrite(m1_f, 0);
+  digitalWrite(m1_b, 1);
+  digitalWrite(m2_f, 0);
+  digitalWrite(m2_b, 1);
+ }
+
+ else if(v == 'r'){
+  digitalWrite(m1_f, 1);
+  digitalWrite(m1_b, 0);
+  digitalWrite(m2_f, 0);
+  digitalWrite(m2_b, 1);
+ }
+
+ else if(v == 'l'){
+  digitalWrite(m1_f, 0);
+  digitalWrite(m1_b, 1);
+  digitalWrite(m2_f, 1);
+  digitalWrite(m2_b, 0);
+ }
+
+ else{
+    digitalWrite(m1_f, 0);
+    digitalWrite(m1_b, 0);
+    digitalWrite(m2_f, 0);
+    digitalWrite(m2_b, 0);
+ }
+ 
+
 }
 }
 
